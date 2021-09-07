3a9c225cb8c07f2c580d63f8551e14fd90502f16
Gustavo Andrade <gustavoandrade15@hotmail.com>
2021-09-07 11:10:29 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,98 @@
+function DataTable() {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>15/04/2021</td>
+                        <td>Carlito Tevez</td>
+                        <td>39</td>
+                        <td>21</td>
+                        <td>16017.00</td>
+                    </tr>
+                    <tr>
+                        <td>01/05/1994</td>
+                        <td>Airton Senna</td>
+                        <td>84</td>
+                        <td>84</td>
+                        <td>39017.00</td>
+                    </tr>
+                    <tr>
+                        <td>29/09/2012</td>
+                        <td>Hebe Camargo</td>
+                        <td>50</td>
+                        <td>49</td>
+                        <td>23017.00</td>
+                    </tr>
+                    <tr>
+                        <td>29/05/2021</td>
+                        <td>Marcos Mion</td>
+                        <td>23</td>
+                        <td>17</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>15/02/2021</td>
+                        <td>Neymar</td>
+                        <td>11</td>
+                        <td>05</td>
+                        <td>5017.00</td>
+                    </tr>
+                    <tr>
+                        <td>05/08/2021</td>
+                        <td>João Dória</td>
+                        <td>01</td>
+                        <td>1</td>
+                        <td>1017.00</td>
+                    </tr>
+                    <tr>
+                        <td>17/02/2021</td>
+                        <td>Jim Query</td>
+                        <td>69</td>
+                        <td>53</td>
+                        <td>38017.00</td>
+                    </tr>
+                    <tr>
+                        <td>23/04/2021</td>
+                        <td>Wesley Safadão</td>
+                        <td>15</td>
+                        <td>09</td>
+                        <td>11017.00</td>
+                    </tr>
+                    <tr>
+                        <td>18/04/2021</td>
+                        <td>Bon Jovi</td>
+                        <td>79</td>
+                        <td>75</td>
+                        <td>75017.00</td>
+                    </tr>
+                    <tr>
+                        <td>09/04/2021</td>
+                        <td>Raul Seixas</td>
+                        <td>63</td>
+                        <td>25</td>
+                        <td>64017.00</td>
+                    </tr>
+                    <tr>
+                        <td>07/09/2021</td>
+                        <td>Nelio Alves</td>
+                        <td>100</td>
+                        <td>100</td>
+                        <td>150017.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
