b7f69790f9b3f7327d0d40b079d4f0ed13d3e026
Gustavo Andrade <gustavoandrade15@hotmail.com>
2021-09-07 12:17:08 -0300

    Update donut and table

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -3,7 +3,7 @@ import Chart from 'react-apexcharts';
 function DonutChart() {
 
     const mockData = {
-        series: [477138, 499928, 444867, 220426, 47308, 100000, 499928, 444867, 220426, 473088, 502088 ],
+        series: [477138, 499928, 444867, 220426, 47308, 50010, 499928, 444867, 220426, 473088, 502088 ],
         labels: ['Carlito Tevez', 'Airton Senna', 'Hebe Camargo', 'Marcos Mion', 'Neymar', 'João Dória', 'Jim Query',
         'Wesley Safadão', 'Bon Jovi', 'Raul Seixas', 'Nelio Alves']
     }
