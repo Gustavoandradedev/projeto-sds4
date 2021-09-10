42170404da22da9ec37885037d2e07d14c6ea5ff
Gustavo Andrade <gustavoandrade15@hotmail.com>
2021-09-09 23:14:56 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendassds4/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsvendassds4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendassds4.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
