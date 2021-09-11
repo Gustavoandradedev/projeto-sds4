e836d92b0ed80c6519aca128b688a69058d67a5a
Gustavo Andrade <gustavoandrade15@hotmail.com>
2021-09-11 17:33:41 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,32 +1,37 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
     labels: string[];
-    series:number[];
+    series: number[];
 }
 
 function DonutChart() {
 
-    //Forma Errada
-    let chartData : ChartData = { labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);  
-            
-            chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData);
-        });
+
+
+
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 47308, 50010, 499928, 444867, 220426, 473088, 502088 ],
-     //   labels: ['Carlito Tevez', 'Airton Senna', 'Hebe Camargo', 'Marcos Mion', 'Neymar', 'João Dória', 'Jim Carrey',
-     //   'Wesley Safadão', 'Bon Jovi', 'Raul Seixas', 'Nelio Alves']
+    //   labels: ['Carlito Tevez', 'Airton Senna', 'Hebe Camargo', 'Marcos Mion', 'Neymar', 'João Dória', 'Jim Carrey',
+    //   'Wesley Safadão', 'Bon Jovi', 'Raul Seixas', 'Nelio Alves']
     //}
 
     const options = {
